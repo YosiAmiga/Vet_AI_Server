25144d42761f4617bd971292598320690563b8d1
itaykarat <itaykarat13@gmail.com>
2023-04-21 13:18:38 +0300

    added funciton that gets an image and returns the predicted label as string

--- Computer_vision/core_classes/emotion_recognition_service/FER_image.py
+++ Computer_vision/core_classes/emotion_recognition_service/FER_image.py
@@ -13,7 +13,7 @@ def load_trained_model(model_path):
     model.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage), strict=False)
     return model
 
-def FER_image(img_path):
+def FER_image(img_path,show_image=False):
 
     model = load_trained_model('./models/FER_trained_model.pt')
     
@@ -23,41 +23,30 @@ def FER_image(img_path):
     val_transform = transforms.Compose([
         transforms.ToTensor()])
 
-
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(img)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        resize_frame = cv2.resize(gray[y:y + h, x:x + w], (48, 48))
-        X = resize_frame/256
-        X = Image.fromarray((resize_frame))
-        X = val_transform(X).unsqueeze(0)
-        with torch.no_grad():
-            model.eval()
-            log_ps = model.cpu()(X)
-            ps = torch.exp(log_ps)
-            top_p, top_class = ps.topk(1, dim=1)
-            pred = emotion_dict[int(top_class.numpy())]
-        cv2.putText(img, pred, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
+    resize_frame = cv2.resize(gray, (48, 48))
+    X = resize_frame/256
+    X = Image.fromarray((resize_frame))
+    X = val_transform(X).unsqueeze(0)
+    with torch.no_grad():
+        model.eval()
+        log_ps = model.cpu()(X)
+        ps = torch.exp(log_ps)
+        top_p, top_class = ps.topk(1, dim=1)
+        pred = emotion_dict[int(top_class.numpy())]
+        # cv2.putText(img, pred, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
+        print(pred)
 
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.grid(False)
-    plt.axis('off')
-    plt.show()
+    if show_image:
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.grid(False)
+        plt.axis('off')
+        plt.show()
 
 
 if __name__ == "__main__":
+    image_path = r"C:\Users\97252\Desktop\Vet_AI_Server\Computer_vision\faces_images\face.png"
+    FER_image(img_path=image_path,show_image = False)
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-p", "--path", required=True,
-        help="path of image")
-    args = vars(ap.parse_args())
-    
-    if not os.path.isfile(args['path']):
-        print('The image path does not exists!!')
-    else:
-        print(args['path'])
-        FER_image(args['path'])
